6b95625cf4e1c619519d5a5e6f7f97728ed22cd2
brvltd <72864426+brvltd@users.noreply.github.com>
2020-11-24 12:49:43 +0300

    updates

--- js/loadData.js
+++ js/loadData.js
@@ -27,17 +27,17 @@ function gotData(data){
 		// infoResults.innerHTML="";
 		infoResults.innerHTML+=`
 		<div class="row">
-		<div class="col-sm-2">
-		<p style="font-size:1vw;" id="tNo">${trackN}</p>
+		<div class="col-sm-2" id="tNos">
+		<p style="font-size:2vw;" id="tNo">${trackN}</p>
 		</div>
-		<div class="col-sm-2">
-		<p style="font-size:1vw;" id="cName">${cargoName}</p>
+		<div class="col-sm-2" id="cNames">
+		<p style="font-size:2vw;" id="cName">${cargoName}</p>
 		</div>
-		<div class="col-sm-2">
-		<p style="font-size:1vw;" id="custNam">${custName}</p>
+		<div class="col-sm-2" id="custNams">
+		<p style="font-size:2vw;" id="custNam">${custName}</p>
 		</div>
-		<div class="col-sm-1">
-		<a style="font-size:1vw;"><i class="fa fa-edit" onclick="editCargo();"></i></a>
+		<div class="col-sm-1" id="edit">
+		<a style="font-size:2vw;"><i class="fa fa-edit" onclick="editCargo();"></i></a>
 		</div>
 		</div>`
 	}
@@ -64,17 +64,17 @@ function gotCustData(data){
 		// infoResults.innerHTML="";
 		infoCustRes.innerHTML+=`
 		<div class="row">
-		<div class="col-md-2">
-		<p style="font-size:1vw;">${PhoneNumber}</p>
+		<div class="col-md-2" id="phone">
+		<p style="font-size:2vw;">${PhoneNumber}</p>
 		</div>
-		<div class="col-md-2">
-		<p style="font-size:1vw;">${CustomerName}</p>
+		<div class="col-md-2" id="cNam">
+		<p style="font-size:2vw;">${CustomerName}</p>
 		</div>
-		<div class="col-md-2">
-		<p style="font-size:1vw;">${Email}</p>
+		<div class="col-md-2" id="em">
+		<p style="font-size:2vw;">${Email}</p>
 		</div>
-		<div class="col-sm-1">
-		<a style="font-size:1vw;"><i class="fa fa-edit"></i></a>
+		<div class="col-sm-1" id="edit">
+		<a style="font-size:2vw;"><i class="fa fa-edit"></i></a>
 		</div>
 		</div>`
 	}
